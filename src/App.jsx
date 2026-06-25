42480a7866c5d92b77247d8a754b419bf633b3bb
Priya <priyadiwane16@gmail.com>
2026-06-25 14:55:07 +0530

    Added recipe slider feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
 import React from "react";
-import "index.css";
+import NavBar from "./components/NavBar";
+import RecipeDetailView from "./components/RecipeDetailView";
+import SearchView from "./components/SearchView";
+import CuisineBar from "./components/Cuisine";
+import HomeView from "./components/HomeView";
+
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 const App = () => {
   return (
-    <div>
-      App
-      <h2>Recipe App</h2>
-    </div>
+    <>
+      <Router>
+        <div className="min-h-screen bg-gray-950 font-sans text-gray-100">
+          <NavBar />
+          <CuisineBar />
+          <Routes>
+            <Route path="/" element={<HomeView />} />
+          </Routes>
+        </div>
+      </Router>
+    </>
   );
 };
 
--- src/components/HomeView.jsx
+++ src/components/HomeView.jsx
@@ -1,7 +1,23 @@
+import React from "react";
+import RecipeSlider from "./RecipeSlider";
+import TrendingRecipe from "./TrendingRecipe";
+import CategorySelection from "./CategorySelection";
+import { API_URL } from "./useFetch";
+
 const HomeView = () => {
   return (
     <>
-      <div>HomeView</div>
+      <main className="max-w-8xl mx-auto px-4 sm:px-6 lg:px-8 py-4">
+        <RecipeSlider
+          title="Staff Curated Picks"
+          fetchUrl={`${API_URL}search.php?f=c`}
+        />
+        <TrendingRecipe
+          title="Quick & Easy Meals"
+          fetchUrl={`${API_URL}filter.php?a=Canadian`}
+        />
+        <CategorySelection />
+      </main>
     </>
   );
 };
